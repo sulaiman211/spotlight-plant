035ac7774986019d7825f456c0cae9c94eea9a06
Sulaiman Marey <adammarey51@gmail.com>
2022-04-13 16:34:50 -0700

    added modles associations to the db

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,23 @@
 //this is the access point for all things database related!
 
-const db = require('./db')
-
-const User = require('./models/User')
+const db = require('./db');
+const Order = require('./models/Order');
+const Product = require('./models/Product');
+const User = require('./models/User');
+const OrderProduct = require('./models/OrderProduct');
 
 //associations could go here!
+Order.belongsTo(User);
+User.hasMany(Order);
 
+Order.belongsToMany(Product, { through: OrderProduct });
+Product.belongsToMany(Order, { through: OrderProduct });
 module.exports = {
   db,
   models: {
     User,
+    Product,
+    Order,
+    orderProduct,
   },
 }
