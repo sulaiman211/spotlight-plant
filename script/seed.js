d8807dea23e89a2147b04986a1d15f1277bd75e2
Sulaiman Marey <adammarey51@gmail.com>
2022-04-13 18:02:57 -0700

    added product to the seed data

--- script/seed.js
+++ script/seed.js
@@ -1,6 +1,11 @@
 'use strict'
 
-const {db, models: {User} } = require('../server/db')
+const {
+  db,
+  models: { User, Product },
+} = require('../server/db');
+const Order = require('../server/db/models/Order');
+const OrderProduct = require('../server/db/models/OrderProduct');
 
 /**
  * seed - this function clears the database, updates tables to
@@ -15,6 +20,79 @@ async function seed() {
     User.create({ username: 'cody', password: '123' }),
     User.create({ username: 'murphy', password: '123' }),
   ])
+  const products = await Promise.all([
+    Product.create({
+      name: 'Lily',
+      imageURL:
+        'https://images.squarespace-cdn.com/content/v1/5ce6d339a8ffbf0001a8fb20/1559326767974-6UG31CD8FBVD689DA8X3/Ecomm_02_Lily_004.jpg?format=750w',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      price: 20,
+    }),
+    Product.create({
+      name: 'Cactus',
+      imageURL:
+        'https://images.squarespace-cdn.com/content/v1/5ce6d339a8ffbf0001a8fb20/1559327121147-RG1ANONPMAAXJKDWRODD/Ecomm_04_Orchid_005.jpg?format=750w',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      price: 30,
+    }),
+    Product.create({
+      name: 'Pencil Plant',
+      imageURL:
+        'https://images.squarespace-cdn.com/content/v1/5ce6d339a8ffbf0001a8fb20/1559327121147-RG1ANONPMAAXJKDWRODD/Ecomm_04_Orchid_005.jpg?format=750w',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      price: 15,
+    }),
+    Product.create({
+      name: 'Alocasia',
+      imageURL:
+        'https://images.squarespace-cdn.com/content/v1/5ce6d339a8ffbf0001a8fb20/1559326925194-SH58FE5Q44V6AO38THEH/Ecomm_05_Alocasia_008.jpg?format=750w',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      price: 20,
+    }),
+    Product.create({
+      name: 'Snake',
+      imageURL:
+        'https://images.squarespace-cdn.com/content/v1/5ce6d339a8ffbf0001a8fb20/1559326971697-R6LD4Q9Z5V1EU4496JRZ/Ecomm_17_Snake_001.jpg?format=750w',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      price: 20,
+    }),
+    Product.create({
+      name: 'Orchid',
+      imageURL:
+        'https://images.squarespace-cdn.com/content/v1/5ce6d339a8ffbf0001a8fb20/1559327121147-RG1ANONPMAAXJKDWRODD/Ecomm_04_Orchid_005.jpg?format=750w',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      price: 35,
+    }),
+    Product.create({
+      name: 'Pink Calathea',
+      imageURL:
+        'https://images.squarespace-cdn.com/content/v1/5ce6d339a8ffbf0001a8fb20/1559327213969-3LILJM6OQFB49I6HBOT8/Ecomm_03_CalatheaPink_006.jpg?format=750w',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      price: 20,
+    }),
+    Product.create({
+      name: 'Maiden Hair',
+      imageURL:
+        'https://images.squarespace-cdn.com/content/v1/5ce6d339a8ffbf0001a8fb20/1559833414098-VOZ0QVGIQDABJ1MZA0I7/Ecomm_13_MaidenHair_006.jpg?format=750w',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      price: 12,
+    }),
+    Product.create({
+      name: 'Palm',
+      imageURL:
+        'https://images.squarespace-cdn.com/content/v1/5ce6d339a8ffbf0001a8fb20/1559833646039-FTYEXWSDNPJG79BCWWQQ/Ecomm_18_Palm_004.jpg?format=750w',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      price: 18,
+    }),
+    Product.create({
+      name: 'Gift Card',
+      imageURL:
+        'https://images.squarespace-cdn.com/content/v1/5ce6d339a8ffbf0001a8fb20/1559834442430-DBRWR0KGWCXAJFOUOG2S/giftcard-template.jpg?format=750w',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      price: 25,
+    }),
+  ]);
+
 
   console.log(`seeded ${users.length} users`)
   console.log(`seeded successfully`)
--- server/db/index.js
+++ server/db/index.js
@@ -18,6 +18,6 @@ module.exports = {
     User,
     Product,
     Order,
-    orderProduct,
+    OrderProduct,
   },
 }
